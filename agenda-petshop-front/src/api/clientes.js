3dde3c66dbf6d91c638711215f4327bc2596a259
alisson sganzerla <alisson_sganzerla@yahoo.com.br>
2020-06-25 18:19:05 -0300

    corrigido query graphql front

--- agenda-petshop-front/src/api/clientes.js
+++ agenda-petshop-front/src/api/clientes.js
@@ -1,32 +1,21 @@
-import { api, opcoesFetch, enderecoApi } from './config'
-
-
+import { api, opcoesFetch } from './config'
 
 const listarClientes = () =>
-
-  fetch(enderecoApi, opcoesFetch('{clientes {nome cpf}})'))
+  fetch('http://localhost:4000/', opcoesFetch('{clientes { nome cpf }}'))
     .then(resposta => resposta.json())
     .then(dados => dados.data.clientes)
 
 const buscarClientePorId = id =>
-  api
-    .get(`/clientes/cliente/${id}`)
-    .then(resposta => resposta.data[0])
+  api.get(`/clientes/cliente/${id}`).then(resposta => resposta.data[0])
 
 const adicionarCliente = cliente =>
-  api
-    .post('/clientes/cliente', cliente)
-    .then(resposta => resposta.data)
+  api.post('/clientes/cliente', cliente).then(resposta => resposta.data)
 
 const alterarCliente = (id, cliente) =>
-  api
-    .put(`/clientes/cliente/${id}`, cliente)
-    .then(resposta => resposta.data)
+  api.put(`/clientes/cliente/${id}`, cliente).then(resposta => resposta.data)
 
 const removerCliente = id =>
-  api
-    .delete(`/clientes/cliente/${id}`)
-    .then(resposta => resposta.data)
+  api.delete(`/clientes/cliente/${id}`).then(resposta => resposta.data)
 
 export default {
   listarClientes,
